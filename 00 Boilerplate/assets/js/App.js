fe1731fbffcef21139ff89fcfa4d26c72f221785
Santiago Camargo <santi.camargo@lemoncode.net>
2017-02-02 11:40:33 +0100

    Removed 00 Boilerplate

--- 00 Boilerplate/assets/js/App.js
+++
@@ -1,6 +0,0 @@
-(function (window) {
-  'use strict';
-
-  var App = {};
-  window.App = App;
-})(window);
--- 00 Boilerplate/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-(function ($, App) {
-  'use strict';
-
-  $(function main() {
-    App.Contacts.run();
-  });
-})(jQuery, window.App);
