2795abeec1ea689473f1afb95b4919ab3711f939
Santiago Camargo <santi.camargo@lemoncode.net>
2017-02-07 18:44:08 +0100

    - Gulp config set up. - Removed ES6 babel preset

--- 01.5 Move To JSX/gulpfile.js
+++ 01.5 Move To JSX/gulpfile.js
@@ -1,20 +1,39 @@
 var path = require('path');
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var del = require('del');
 
-var buildDir = path.resolve(__dirname, 'assets', 'js');
+var buildDir = path.resolve(__dirname, 'src');
+var distDir = path.resolve(__dirname, 'dist');
 
 gulp.task('transpile', function () {
-  gulp
+  return gulp
     .src(path.resolve(buildDir, '**', '*.jsx'))
     .pipe(babel({
       presets: ['react']
     }))
-    .pipe(gulp.dest(buildDir));
+    .pipe(gulp.dest(distDir));
 });
 
-gulp.task('build:watch', function () {
-  gulp.watch(path.resolve(buildDir, '**', '*.jsx'), ['transpile']);
+gulp.task('copy', function () {
+  return gulp
+    .src([
+      path.resolve(buildDir, '**', '*.css'),
+      path.resolve(buildDir, '**', '*.js')
+    ])
+    .pipe(gulp.dest(distDir))
 });
 
-gulp.task('default', ['build:watch']);
+gulp.task('clean', function (done) {
+  return del(distDir, done);
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('copy', 'transpile')));
+
+gulp.task('watch', function () {
+  gulp.watch(path.resolve(buildDir, '**', '*.css'), gulp.series('copy'));
+  gulp.watch(path.resolve(buildDir, '**', '*.js'), gulp.series('copy'));
+  gulp.watch(path.resolve(buildDir, '**', '*.jsx'), gulp.series('transpile'));
+});
+
+gulp.task('default', gulp.parallel('build', 'watch'));
