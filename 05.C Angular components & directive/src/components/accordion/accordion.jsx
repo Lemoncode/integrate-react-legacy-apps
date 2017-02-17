24743e41c1c0d283a0cbbe66e3c98e1ead30d7c4
Santiago Camargo <santi.camargo@lemoncode.net>
2017-02-17 14:25:38 +0100

    Remove unnecessary comments.

--- 05.C Angular components & directive/src/components/accordion/accordion.jsx
+++ 05.C Angular components & directive/src/components/accordion/accordion.jsx
@@ -44,38 +44,4 @@
     .module('app')
     .factory('Accordion', ['AccordionPanel', Accordion]);
 })(window.React, window.angular);
-/*
-(function (angular) {
-  function AccordionController() {
-    var self = this;
-    var panels = [];
 
-    self.addPanel = function (panel) {
-      panels.push(panel);
-      if (panels.length) {
-        panels[0].show();
-      }
-    };
-
-    self.select = function (selectedPanel) {
-      panels.forEach(function (panel) {
-        if (panel === selectedPanel) {
-          panel.show();
-        } else {
-          panel.hide();
-        }
-      });
-    };
-  }
-
-  angular
-    .module('app')
-    .component('accordion', {
-      bindings: {
-        feeds: '<'
-      },
-      templateUrl: './dist/components/accordion/accordion.html',
-      controller: AccordionController
-    });
-})(window.angular);
-*/
